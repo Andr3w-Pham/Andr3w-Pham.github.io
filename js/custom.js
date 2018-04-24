aef2c6d40c918ecc5f408ac02b402fffd7515673
Andrew <andrew.pham2492@gmail.com>
2018-04-24 12:25:11 +1000

    my-portfolio

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+function myFunction1() {
+   document.getElementById("info-html5").innerHTML = "HTML5 is the latest version of Hypertext Markup Language, we use it to structure and present content on the World Wide Web (WWW).";
+}
+
+function myFunction2() {
+   document.getElementById("info-html5").innerHTML = "";
+}
