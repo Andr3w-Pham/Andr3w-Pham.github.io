a0a0aba5a30bc63c4f670295f13427afb5c0f397
Andrew <andrew.pham2492@gmail.com>
2018-04-28 21:37:17 +1000

    update

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,21 @@
-function myFunction1() {
-   document.getElementById("info-html5").innerHTML = "HTML5 is the latest version of Hypertext Markup Language, we use it to structure and present content on the World Wide Web (WWW).";
-}
+/*========================================================================
+                                SKILLS
+========================================================================== */
+/*
+$(document).ready(function() {
 
-function myFunction2() {
-   document.getElementById("info-html5").innerHTML = "";
-}
+
+});
+
+jQuery document.ready method.It's used to make a function available after the page is ready
+It is good place to have all the jQuery events & functions inside this document.ready method.
+
+*/
+
+/* Equivalent to recommended way of calling document.ready() method */
+$(function() {
+  // animate on scroll
+  new WOW().init();
+
+
+});
